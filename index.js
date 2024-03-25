6a05f154ba9a2d5d04bf923082ba57b71580f9de
Nikifor Dragov <ndragov99@gmail.com>
2024-03-25 10:56:58 +0200

    Add styles

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 app.listen(port, () => {
